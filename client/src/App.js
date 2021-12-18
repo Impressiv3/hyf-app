ff28543c69066230af5dec19a67a0480cb2ddaeb
Infamou5 <oreal_b52@hotmail.com>
2021-12-18 17:04:33 +0100

    App.js updated

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,12 @@
 import "./App.css";
+import React from "react";
+import image from "./assets/media/images/BalintPhoto.jpg";
 
 export default function App() {
   return (
     <div className='App'>
       <h1>ME REACT APP HELLO</h1>
+      <img src={image} alt='Balint'></img>
     </div>
   );
 }
