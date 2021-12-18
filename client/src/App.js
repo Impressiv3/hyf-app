02d1771a43a036e5459f3e0146d67748b41f3669
Infamou5 <oreal_b52@hotmail.com>
2021-12-18 21:19:36 +0100

    changed public folder

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,10 @@
 import "./App.css";
 import React from "react";
-import image from "./assets/media/images/BalintPhoto.jpg";
 
 export default function App() {
   return (
     <div className='App'>
-      <h1>ME REACT APP HELLO</h1>
-      <img src={image} alt='Balint'></img>
+      <h1>ME REACT APP B52</h1>
     </div>
   );
 }
