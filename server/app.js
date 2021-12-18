3dfdef63b95a8d317ea9eb0da22f2df55bd78332
Infamou5 <oreal_b52@hotmail.com>
2021-12-18 21:44:30 +0100

    updated app.js,added cors

--- server/app.js
+++ server/app.js
@@ -2,17 +2,24 @@ const createError = require("http-errors");
 const express = require("express");
 const path = require("path");
 const helmet = require("helmet");
+const cors = require("cors");
 const compression = require("compression");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 
 const app = express();
 
+const corsOptions = {
+  origin: "http://localhost:8081",
+};
+
+app.use(cors(corsOptions));
+
 app.use(helmet("dev"));
 app.use(compression());
 app.use(logger("dev"));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 app.use(express.static(path.join(__dirname, "public")));
