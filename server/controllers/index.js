9152258a8c3d819f322420bd82fc4286b75b58c9
Infamou5 <oreal_b52@hotmail.com>
2021-12-23 13:13:14 +0100

    Api update

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,11 +1,11 @@
-/* const { Meal } = require("../models"); */
-const { Meal } = require("../database/database.js")
+const { Meal } = require("../models"); 
+/* const { Meal } = require("../database/database.js") */
 
 const createMeal = async (req, res) => {
   try {
-    const user = await Meal.create(req.body);
+    const meal = await Meal.create(req.body);
     return res.status(201).json({
-      user,
+      meal,
     });
   } catch (error) {
     return res.status(500).json({ error: error.message });
